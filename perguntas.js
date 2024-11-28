6cf6f1e03cebb6834e554a3e022d3c5266a5c004
Wallace10GR2 <gr2mzs@gmail.com>
2024-11-28 11:05:16 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Quais são as principais características do Python?',
+    'O Python é conhecido por sua simplicidade, legibilidade e ampla versatilidade.'
+)
+
+criaCartao(
+    'Geografia',
+    'Que cidade é conhecida como a "Cidade Luz"?',
+    'A cidade conhecida como "Cidade Luz" é Paris.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a finalidade de uma função?',
+    'A função serve para agrupar código reutilizável que executa uma tarefa específica.'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Qual é a tradução de "Good morning"?',
+    'A tradução de "Good morning" é "Bom dia".'
+)
